e4f4e2e3284630ab0745112ca853647bab00f9ae
Ansonsd5 <anson11sd@gmail.com>
2022-03-02 21:01:35 +0530

    all done
    good to go

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#input-text");
-var outputDev = document.querySelector("#output");
+var outputDev = document.querySelector("#output-area");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+function errorHandler(error) {
+    if (
+        error ==
+        "TypeError: Cannot read properties of undefined (reading 'translated')"
+    ) {
+        alert("Please wait for an hour and try later!");
+    } else {
+        alert("Error : " + error);
+    }
+}
+
 function getserverURL(text) {
+   
 
     return serverURL + "?" + "text=" + text;
-}
+
+    }
+
+
+
 
 function clickHandler() {
     var textinput = inputText.value;
     fetch(getserverURL(textinput))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText=json.contents.translated;
-        outputDev.innerText=translatedText;
-    })
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDev.innerText = translatedText;
 
+        })
+
+        .catch(errorHandler)
 }
 
-btnTranslate.addEventListener("click", clickHandler)
+
+btnTranslate.addEventListener("click", clickHandler())
